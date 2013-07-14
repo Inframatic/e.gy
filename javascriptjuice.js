c8b76f7ada6679c14a407b0965fc6b2faaa714ee
Inframatic <isteffy@hotmail.com>
2013-07-13 20:54:19 -0400

    Moved zoom to bottom right. float the header

--- javascriptjuice.js
+++ javascriptjuice.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
 		function() { $(this).animate( { opacity: 0.8, marginTop: '5px' }, "fast" ); },
 		function() { $(this).animate( { opacity: 1.0, marginTop: '0px' }, "fast" ); }
 	);
+
+
+	var cloudmadeUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
+	var subDomains = ['otile1','otile2','otile3','otile4'];
+	var cloudmadeAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+	var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, minZoom: 3, attribution: cloudmadeAttrib, subdomains: subDomains, zindex: -1});
+	var latlng = new L.LatLng(85.037, -167.343);
+	var maxBounds = new L.LatLngBounds(latlng);
+	var london = new L.LatLng(51.505, -0.09);
+	var map = new L.Map('map', {center: london, zoom: 13, zoomControl: false, layers: [cloudmade]});
+	new L.Control.Zoom({position: "bottomright"}).addTo(map)
 });
