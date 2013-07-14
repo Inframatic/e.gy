8af9f75b4dad68820f651ac20c97d469c0cb8223
Inframatic <isteffy@hotmail.com>
2013-07-14 14:30:44 -0400

    Header is A-ok. Email/name box is good too

--- javascriptjuice.js
+++ javascriptjuice.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
 	var cloudmadeUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
 	var subDomains = ['otile1','otile2','otile3','otile4'];
-	var cloudmadeAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+	var cloudmadeAttrib = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 	var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, minZoom: 3, attribution: cloudmadeAttrib, subdomains: subDomains, zindex: -1});
 	var latlng = new L.LatLng(85.037, -167.343);
 	var maxBounds = new L.LatLngBounds(latlng);
